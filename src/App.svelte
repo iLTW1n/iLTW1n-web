200676909c656a7e4c4cc7a8e18a30b9b2589b81
iLTW1n <jose.osk01@gmail.com>
2021-07-12 22:43:39 -0500

    Start build personal web

---
+++ src/App.svelte
@@ -0,0 +1,8 @@
+<script>
+	import Home from './pages/home.svelte';
+	import About from './pages/about.svelte';
+
+	export let url = '';
+</script>
+
+<div>dasdasdas</div>
---
+++ src/pages/about.svelte
@@ -0,0 +1,5 @@
+<script>
+	export let location;
+</script>
+
+<div>About</div>
