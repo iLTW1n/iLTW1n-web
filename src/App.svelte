6b69c2cdd3eb035548de83717b6cddf2d8052761
iLTW1n <jose.osk01@gmail.com>
2021-10-09 23:51:05 -0500

    Refactoring projects

--- src/App.svelte
+++ src/App.svelte
@@ -7,7 +7,7 @@
 
 	onMount(() => {
 		if (process.env.production) {
-			scriptSrc = 'https://www.googletagmanager.com/gtag/js?id=G-SQEJPBLJ3E';
+			// scriptSrc = 'https://www.googletagmanager.com/gtag/js?id=G-SQEJPBLJ3E';
 		}
 	})
 </script>
