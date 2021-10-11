a5015f15d004a966e3b022c9f041edee775568ae
iLTW1n <jose.osk01@gmail.com>
2021-10-10 22:10:54 -0500

    Refactoring projects

--- src/App.svelte
+++ src/App.svelte
@@ -1,28 +1,6 @@
 <script>
 	import { Router } from '@roxi/routify';
 	import { routes } from '../.routify/routes';
-	import { onMount } from "svelte";
-
-	let scriptSrc;
-
-	onMount(() => {
-		if (process.env.production) {
-			// scriptSrc = 'https://www.googletagmanager.com/gtag/js?id=G-SQEJPBLJ3E';
-		}
-	})
 </script>
-
-<svelte:head>
-	{#if scriptSrc}
-		<script async src='{scriptSrc}'></script>
-		<script>
-			window.dataLayer = window.dataLayer || [];
-			function gtag(){dataLayer.push(arguments);}
-			gtag('js', new Date());
-
-			gtag('config', 'G-SQEJPBLJ3E');
-		</script>
-	{/if}
-</svelte:head>
 
 <Router { routes } />
